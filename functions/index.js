8d97c27b7e36b6b85d009cdea8abf2ccc3f265cd
Brian Pham <br.pham13@gmail.com>
2023-05-08 17:25:04 -0700

    Test deploy functions to Proov App project v2

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
